8c451a54b9571c49f0825b4492a79535618ff995
KrutikaYewale <krutikakishoryewale@gmail.com>
2026-05-26 21:24:17 +0530

    Add files via upload
    DSA-day-1

---
+++ DAY_1/insertion_exertion.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {    
+    
+    int age = 0 ,marks = 0;
+    cout << "Enter your age:"<<endl;
+    cin >> age;
+    cout << "Enter your marks:"<<endl;
+    cin >> marks;
+
+    return 0;
+}
